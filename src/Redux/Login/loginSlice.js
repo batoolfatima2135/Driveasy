fb690870b5d5fc123982e1542ef3832695ce2859
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-26 16:46:06 +0500

    Add login Slice

---
+++ src/Redux/Login/loginSlice.js
@@ -0,0 +1,45 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const loginUser = createAsyncThunk('greetings/fetchGreetings', async (username) => {
+  try {
+    const response = await fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username }),
+    });
+    const message = await response.json();
+    return message;
+  } catch (error) {
+    return error.message;
+  }
+});
+
+const initialState = {
+  loading: false,
+  error: null,
+  message: null,
+};
+
+const login = createSlice({
+  name: 'login',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.message = action.payload.message;
+        state.loading = false;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.error = action.payload.message;
+        state.loading = false;
+      });
+  },
+});
+
+export default login.reducer;
