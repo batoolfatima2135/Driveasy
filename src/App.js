6b043f74e01d23985acacf69f86327d49bde2770
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-26 18:08:42 +0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,14 @@
-import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Car from './Components/Car/Car';
+import Login from './Components/Login/Login';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username);
-  };
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
   return (
-    <div
-      className="h-screen flex items-center justify-center"
-      style={{
-        backgroundImage: 'url("https://purepng.com/public/uploads/large/purepng.com-suzukisuzukisuzuki-carsuzuki-automobilessuzuki-carsvehicles-1701527677135nhb1t.png")',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-      }}
-    >
-      <div className="h-screen flex items-center justify-center  bg-custom-green bg-opacity-95 w-full">
-        <div className="lg:w-1/3 w-4/5">
-          <h2 className="text-2xl w-3/4  font-bold text-center mx-auto text-white tracking-widest  mb-4 pb-4 font-sans border-b">LOGIN </h2>
-          <p className="text-center text-white m-4">Welcome to the Driveasy, to Continue to the website kindly enter a unique username.</p>
-          <div className="mb-4">
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={username}
-              onChange={handleUsernameChange}
-              className="outline-none w-full border rounded-full py-3 px-5"
-              placeholder="Enter Username"
-            />
-          </div>
-          <div className="flex justify-center ">
-            <button type="submit" onClick={handleSubmit} className="bg-white m-4 font-medium text-custom-green w-1/3 py-3 rounded-full hover:bg-custom-green-light hover:text-white focus:outline-none focus:ring focus:ring-white">
-              LOGIN
-            </button>
-          </div>
-        </div>
-
-      </div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cars" element={<Car />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './Redux/store';
 import './index.css';
 import App from './App';
@@ -8,12 +9,15 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+      ,
+    </Provider>
     ,
-  </Provider>,
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
