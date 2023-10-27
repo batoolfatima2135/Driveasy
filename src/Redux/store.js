e077376dafa515d9ca3f5d4dfd7dccfc733b3fe6
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-27 13:53:34 +0500

    Add car slice

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './Login/loginSlice';
+import carReducer from './Car/carSlice';
 
 const store = configureStore({
   reducer: {
     login: loginReducer,
+    car: carReducer,
   },
 });
 
