da5808447d6c0479ab7b49498fd7016152d037f5
othniel2471 <othnielomaidu@gmail.com>
2023-10-27 23:32:04 +0100

    added route and path for add cars

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Car from './Components/Car/Car';
 import Login from './Components/Login/Login';
 import CarDetails from './Components/Car/CarDetails';
 import SideBar from './Components/Layout/SideBar';
+import AddForm from './Components/Car/AddForm';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/cars" element={<Car />} />
         <Route path="/car/details" element={<CarDetails />} />
+        <Route path="/car/add" element={<AddForm />} />
       </Routes>
     </div>
   );
