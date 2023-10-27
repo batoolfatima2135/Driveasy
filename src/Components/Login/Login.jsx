d4fbed55c1082d6167cea0e88ef65386a94da0bb
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-27 13:55:47 +0500

    Update login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../../Redux/Login/loginSlice';
 
 export default function Login() {
   const [username, setUsername] = useState('');
-
+  const [hasNavigated, setHasNavigated] = useState(false);
   const status = useSelector((state) => state.login.status);
-  const message = useSelector((state) => state.login.message);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(loginUser(username));
@@ -18,10 +18,13 @@ export default function Login() {
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
   };
-  if (status === 'created') {
-    navigate('/cars');
-  }
 
+  useEffect(() => {
+    if (status === 'login' && !hasNavigated) {
+      setHasNavigated(true);
+      navigate('/cars');
+    }
+  }, [status, navigate, hasNavigated]);
   return (
     <div
       className="h-screen flex items-center justify-center"
@@ -35,8 +38,6 @@ export default function Login() {
         <div className="lg:w-1/3 w-4/5">
           <h2 className="text-2xl w-3/4  font-bold text-center mx-auto text-white tracking-widest  mb-4 pb-4 font-sans border-b">LOGIN </h2>
           <p className="text-center text-white m-4">Welcome to the Driveasy, to Continue to the website kindly enter a unique username.</p>
-          {status === 'not unique'
-           && <p className="text-center text-red-600 m-1">{message}</p>}
           <div className="mb-4">
             <input
               type="text"
