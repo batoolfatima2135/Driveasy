d8b8f8c6ebf4f32199253b5a4d150482ad7df2a5
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-30 18:42:33 +0500

    Add slice for car uploading

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './Login/loginSlice';
 import carDetailsReducer from './Car/carDetailsSlice';
+import carAddReducer from './Car/carAddSlice';
 
 const store = configureStore({
   reducer: {
     login: loginReducer,
     carData: carDetailsReducer,
+    addCar: carAddReducer,
   },
 });
 
