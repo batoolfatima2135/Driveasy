187385759453bba99b21268d8df7ff6c52c7c7b6
othniel2471 <othnielomaidu@gmail.com>
2023-10-30 16:16:54 +0100

    added carSlice to store

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from './Login/loginSlice';
 import carDetailsReducer from './Car/carDetailsSlice';
+import carSliceReducer from './Car/carSlice';
 
 const store = configureStore({
   reducer: {
     login: loginReducer,
     carData: carDetailsReducer,
+    cars: carSliceReducer,
   },
 });
 
