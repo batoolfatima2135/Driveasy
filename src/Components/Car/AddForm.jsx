6719cb44c63c2d95a2504b7d730f243f46a81aef
Batool Fatima <batoolfatima2135@gmail.com>
2023-10-30 18:41:16 +0500

    Set data from slice into state

--- src/Components/Car/AddForm.jsx
+++ src/Components/Car/AddForm.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ColorRing } from 'react-loader-spinner';
+import { addCar } from '../../Redux/Car/carAddSlice';
 
 const MyForm = () => {
+  const status = useSelector((state) => state.addCar.message);
+  const loading = useSelector((state) => state.login.loading);
+  const dispatch = useDispatch();
+
   const [formData, setFormData] = useState({
     name: '',
     price: '',
@@ -30,9 +37,15 @@ const MyForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    dispatch(addCar(formData));
+    setFormData({
+      name: '',
+      price: '',
+      color: '',
+      model: '',
+      image: null, // For file input
+    });
   };
-
   return (
     <div className="min-h-screen flex items-center justify-center ml-[0.1em] md:max-w-md md:mx-auto max-w-[16rem]">
       <div>
@@ -73,6 +86,7 @@ const MyForm = () => {
                 name="name"
                 value={formData.name}
                 onChange={handleChange}
+                required
                 className="w-full border-b outline-none rounded p-2"
               />
             </label>
@@ -84,6 +98,7 @@ const MyForm = () => {
                 id="price"
                 type="number"
                 name="price"
+                required
                 value={formData.price}
                 onChange={handleChange}
                 className="w-full border-b outline-none rounded p-2"
@@ -97,6 +112,7 @@ const MyForm = () => {
                 id="color"
                 type="text"
                 name="color"
+                required
                 value={formData.color}
                 onChange={handleChange}
                 className="w-full border-b outline-none rounded p-2"
@@ -110,12 +126,28 @@ const MyForm = () => {
                 id="model"
                 type="text"
                 name="model"
+                required
                 value={formData.model}
                 onChange={handleChange}
                 className="w-full border-b outline-none rounded p-2"
               />
             </label>
           </div>
+          {loading && (
+            <div className="flex justify-center align-middle">
+              <ColorRing
+                visible
+                height="50"
+                width="50"
+                className="m-2"
+                ariaLabel="blocks-loading"
+                wrapperStyle={{}}
+                wrapperClass="blocks-wrapper"
+                colors={['#FFFFFF', '#EDEADE', '#F9F6EE', '#FFF8DC', '#FFFFF0', '#FAF9F6']}
+              />
+            </div>
+          )}
+          {!loading && status && (<p className="text-green-800 text-center">{status}</p>)}
           <div className="flex items-center justify-center">
             <button
               type="submit"
