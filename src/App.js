a588df7537b20714da1d2841ff89d5ed51321c3c
othniel2471 <othnielomaidu@gmail.com>
2023-10-31 11:45:02 +0100

    updated the styles for mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
-import Car from './Components/Car/Car';
+import CarPage from './Pages/CarPage';
 import Login from './Components/Login/Login';
 import CarDetails from './Components/Car/CarDetails';
 import SideBar from './Components/Layout/SideBar';
@@ -7,24 +7,28 @@ import AddForm from './Components/Car/AddForm';
 
 function App() {
   const location = useLocation();
-
   const showSideBar = location.pathname !== '/';
 
+  const sidebarStyle = showSideBar ? { width: '25%' } : { width: '0' };
+  const contentStyle = showSideBar
+    ? { width: '70%', marginLeft: '-1rem' }
+    : { width: '100%' };
+
   return (
-    <div
-      className={`${
-        showSideBar ? 'flex gap-6 overflow-auto' : 'gap-6 overflow-auto'
-      }`}
-    >
-      {showSideBar && <SideBar />}
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/cars" element={<Car />} />
-        <Route path="/car/details" element={<CarDetails />} />
-        <Route path="/car/add" element={<AddForm />} />
-      </Routes>
+    <div style={{ display: 'flex', width: '100%' }}>
+      <div style={sidebarStyle}>{showSideBar && <SideBar />}</div>
+      <div style={contentStyle} className="">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/cars" element={<CarPage />} />
+          <Route path="/car/details/:id" element={<CarDetails />} />
+          <Route path="/car/add" element={<AddForm />} />
+        </Routes>
+      </div>
     </div>
   );
 }
+
+// export default App;
 
 export default App;
