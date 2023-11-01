6339d353b5bbe0839aa7003b26ff1a482960650e
othniel2471 <othnielomaidu@gmail.com>
2023-11-01 15:53:42 +0100

    added reservation slice

---
+++ src/Redux/Reservation/reservationSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchReservations = createAsyncThunk(
+  'reservations/fetchReservations',
+  async (formData) => {
+    const response = await fetch('http://localhost:3000/reservations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await response.json();
+    return data;
+  },
+);
+
+const initialState = {
+  reservation: null,
+  loading: false,
+  error: null,
+};
+
+const reservationSlice = createSlice({
+  name: 'reservations',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchReservations.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchReservations.fulfilled, (state, action) => {
+        state.loading = false;
+        state.reservation = action.payload;
+      })
+      .addCase(fetchReservations.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default reservationSlice.reducer;
